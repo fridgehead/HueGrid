1918f3d5726496eca8e0b0679c6f55036b2788f2
Benjamin Blundell <oni@section9.co.uk>
2014-11-24 20:20:10 +0000

    Minor fix

--- Server.py
+++ Server.py
@@ -80,19 +80,22 @@ class GridServer:
 
         idx = 0
         ridx = 0
+        cidx = 29
    
-        for i in range(0,30):
-          if i > 30 - self.bufferY:
+        for i in range(0,30)):
+          if i < self.bufferY:
             for j in range(0,30):
               if j < self.bufferX:
                 (r,g,b) = palette[int(received_buffer[ridx])]
 
-                self.led_data[idx*3] = b
-                self.led_data[idx*3 + 1] = r
-                self.led_data[idx*3 + 2] = g
+                self.led_data[cidx * 30 + idx] = b
+                self.led_data[cidx * 30 + idx + 1] = r
+                self.led_data[cidx * 30 + idx + 2] = g
               
                 ridx+=1
               idx += 1
+            idx = 0
+            cidx = cidx - 1
     
 
       # Check against rate limit - dont overload
